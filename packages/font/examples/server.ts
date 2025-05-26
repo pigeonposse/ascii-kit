604d43da398231662f551488ec3f013bca804135
angelespejo <angelespejo13@gmail.com>
2025-05-26 22:13:59 +0200

    :sparkles: feat(env): fix script

--- packages/font/examples/server.ts
+++ packages/font/examples/server.ts
@@ -11,33 +11,6 @@ Bun.serve( {
 
 		} },
 	},
-	// async fetch( req ) {
-
-	// 	const url = new URL( req.url )
-
-	// 	// Manejar petición para el JSON con proxy CORS
-	// 	if ( url.pathname === '/fonts' ) {
-
-	// 		// Hace fetch al recurso original en unpkg
-	// 		const response = await fetch( 'https://unpkg.com/@ascii-kit/fonts/dist/list.json' )
-	// 		// const data     = await response.json()
-
-	// 		return response
-
-	// 	}
-
-	// 	if ( req.method === 'OPTIONS' ) {
-
-	// 		const res = new Response( null, { status: 204 } )
-	// 		res.headers.set( 'Access-Control-Allow-Origin', '*' )
-	// 		res.headers.set( 'Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS' )
-	// 		return res
-
-	// 	}
-
-	// 	return new Response( homepage, { headers: { 'Content-Type': 'text/html' } } )
-
-	// },
 	fetch( req ) {
 
 		const url = new URL( req.url )
